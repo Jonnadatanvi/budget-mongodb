f269613bdda33fbb6e7140c3dd706ffd44b5cc1e
Jonnadatanvi <tjonnada@uncc.edu>
2023-10-25 21:05:50 -0400

    Added Fetching data from MongoDB

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const BudgetItem = require('./budgetItem'); // Import the Mongoose model
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
-
-const budget = require('./budget.json');
-
-app.get('/budget', (req, res) => {
-    res.json(budget);
+mongoose.connect('mongodb://localhost:27017/myBudgetDatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-app.get('/hello', (req,res) => {
-    res.send('Hello World');
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/budget', async (req, res) => {
+  try {
+    console.log('Received POST request:', req.body);
+    const budgetItems = await BudgetItem.find();
+    res.json({ myBudget: budgetItems });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
+
+app.post('/budget', async (req, res) => {
+    try {
+      const newItem = req.body;
+      const budgetItem = new BudgetItem(newItem);
+      await budgetItem.save();
+      res.status(201).json(budgetItem);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });  
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
